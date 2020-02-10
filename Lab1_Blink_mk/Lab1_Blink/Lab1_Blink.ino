8eaf00ad47b56cdf83d5a1fb5fb5929ff3e86858
Maryn Kunthara <kunthara.maryn@utexas.com>
2020-02-10 17:59:50 -0600

    final Part A

---
+++ Lab1_Blink_mk/Lab1_Blink/Lab1_Blink.ino
@@ -0,0 +1,31 @@
+#define LED_PIN 2
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+  // initialize digital pin LED_PIN as an output.
+  pinMode(LED_PIN, OUTPUT);
+}
+
+void dimmer(int freq, int duty) {
+    int period, onTime, offTime;
+    period = 1000/freq;
+    onTime = period * duty / 100;
+    offTime = period - onTime;
+    digitalWrite(LED_PIN, HIGH);
+    delay(onTime);
+    digitalWrite(LED_PIN, LOW);
+    delay(offTime);
+}
+
+void timedBlink(int interval) {
+  digitalWrite(LED_PIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay (interval);                       // wait for a second
+  digitalWrite(LED_PIN, LOW);    // turn the LED off by making the voltage LOW
+  delay(interval); 
+}
+// the loop function runs over and over again forever
+void loop() {
+  timedBlink(250);
+  timedBlink(500);
+  timedBlink(1000);
+}
